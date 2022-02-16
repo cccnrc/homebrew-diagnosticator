17bf37ef28c02b148c873aaf99ce1d45bf6037dd
cccnrc <48554373+cccnrc@users.noreply.github.com>
2022-02-16 15:43:44 -0800

    Update diagnosticator.rb

--- diagnosticator.rb
+++ diagnosticator.rb
@@ -1,8 +1,8 @@
 class Diagnosticator < Formula
   desc "Diagnosticator Mac OS package"
   homepage "https://diagnosticator.com"
-  url "https://github.com/cccnrc/diagnosticator-mac/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "27e8cbdfee22284f5295c9f7da7842a27fcb84fcdd0d6521e117f99a070a1885"
+  url "https://github.com/cccnrc/diagnosticator-mac/archive/refs/tags/v0.1.4.tar.gz"
+  sha256 "b1e644ed5c22c5ee88485b8dcbf45d9b88d9103eba835c49a035ccb55adc776c"
   license "MIT"
 
   depends_on "docker"
