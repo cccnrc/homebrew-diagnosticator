d5cbf5f47b6234ce09f886abb7020405ff6de056
cccnrc <48554373+cccnrc@users.noreply.github.com>
2022-02-16 14:21:44 -0800

    Create diagnosticator-mac.rb

---
+++ diagnosticator-mac.rb
@@ -0,0 +1,21 @@
+class DiagnosticatorMac < Formula
+  desc "Diagnosticator Mac OS package"
+  homepage "https://diagnosticator.com"
+  url "https://github.com/cccnrc/diagnosticator-mac/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "3289d3f016ffd6799f802cf2d45c8913457923dad4c1788ce6af917cf38527ba"
+  license "MIT"
+
+  depends_on "docker"
+  depends_on "docker-compose"
+  depends_on "wget"
+  depends_on "curl"
+  depends_on "jq"
+  depends_on "printf"
+  depends_on "cmd"
+
+  def install
+    bin.install diagnosticator
+    bin.install Dir["files"]
+    prefix.install "README.md"
+  end
+end
