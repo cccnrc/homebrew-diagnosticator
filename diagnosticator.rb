ecd430088d6b627f3049ef50826cee0a59db37db
cccnrc <48554373+cccnrc@users.noreply.github.com>
2022-02-16 15:56:14 -0800

    Update diagnosticator.rb

--- diagnosticator.rb
+++ diagnosticator.rb
@@ -1,5 +1,5 @@
 class Diagnosticator < Formula
-  desc "Diagnosticator Mac OS package"
+  desc "Diagnosticator Mac OS homebrew package"
   homepage "https://diagnosticator.com"
   url "https://github.com/cccnrc/diagnosticator-mac/archive/refs/tags/v0.1.5.tar.gz"
   sha256 "62ce220ed6c5b43a94f1d6eae6a970e19d558334ab180bdae642cd880c4244f6"
